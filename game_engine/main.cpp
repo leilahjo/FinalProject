cf01b5e28c01d17e9bbbf657ac6f8fece8b9e2d1
leilahjo <mandyinthejungle@gmail.com>
2026-01-22 11:02:29 -0500

    Always print diagnostic info.

--- game_engine/main.cpp
+++ game_engine/main.cpp
@@ -19,7 +19,7 @@ int main()
         DrawText(TextFormat("FPS %f | Jitter: %lld us | Work: %lld us", frameData.fps, frameData.jitterUs,
                             frameData.workDurationUs), 100, 100, 30, DARKGRAY);
         EndDrawing();
-        if (frameData.workDurationUs > 16'666)
+        //if (frameData.workDurationUs > 16'666)
         {
             std::cout << "FPS " << frameData.fps << " | jitter: " << frameData.jitterUs << " us | work : " << frameData.
                 workDurationUs << "us\n";
