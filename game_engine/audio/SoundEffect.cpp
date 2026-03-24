32857100c1e1fee52a0204b9121fc626878c218c
leilahjo <mandyinthejungle@gmail.com>
2026-03-24 14:22:11 -0400

    Implement voices

--- game_engine/audio/SoundEffect.cpp
+++ game_engine/audio/SoundEffect.cpp
@@ -11,15 +11,21 @@ namespace Audio
 {
     void SoundEffect::load(const std::string& path, uint8_t maxVoices, VoiceExhaustionBehavior behavior)
     {
-        voices.resize(1);
+        voices.resize(maxVoices);
         voices[0] = LoadSound(path.c_str());
+        for (size_t i = 0; i < maxVoices; i++)
+            voices[i] = LoadSoundAlias(voices[0]);
 
         voiceExhaustionBehavior = behavior;
     }
 
     bool SoundEffect::playOneShot()
     {
-        PlaySound(voices[0]);
+        auto& nextVoice = voices[nextVoiceIndex];
+        if (IsSoundPlaying(nextVoice) && voiceExhaustionBehavior == DROP)
+            return false;
+        PlaySound(nextVoice);
+        nextVoiceIndex = (nextVoiceIndex + 1) % voices.size();
         return true;
     }
 }
