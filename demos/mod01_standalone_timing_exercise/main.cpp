ff38c43d063bfac37d5562f5e3be14517f96a8b7
leilahjo <mandyinthejungle@gmail.com>
2026-04-17 18:30:00 -0600

    Remove CLion help comment from main.cpp

--- demos/mod01_standalone_timing_exercise/main.cpp
+++ demos/mod01_standalone_timing_exercise/main.cpp
@@ -19,5 +19,4 @@ int main()
     std::cout << "Duration (ns): " << (end - start) / 1'000'000 << std::endl;
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
